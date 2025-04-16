4ce45d5b82d174e48e400f2da2a33a441eb26fb3
ricky firmansyah <rickyfirmansyah27@gmail>
2025-04-16 23:33:47 +0700

    add no rount handler

--- backend/routes/index.py
+++ backend/routes/index.py
@@ -1,4 +1,5 @@
 from fastapi import APIRouter
+from helper.baseResponse import BaseResponse
 from controller.data_controller import router as data_router
 # from controller.ai_controller import router as ai_router
 
@@ -8,3 +9,7 @@ base_urls = "/api"
 
 router.include_router(data_router, prefix=base_urls)
 # router.include_router(ai_router, prefix=base_urls)
+
+@router.api_route("/{full_path:path}", methods=["GET", "POST", "PUT", "DELETE", "PATCH"], tags=["data"])
+async def not_route_found(full_path: str):
+    return BaseResponse('notfound', 'No Route Found')
